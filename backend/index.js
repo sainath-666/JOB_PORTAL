29dbbaada8f46f5971eb14729a1be72e84a09f1d
Sainathreddy <sai65265@gmail.com>
2025-07-15 05:55:59 +0530

    completed job controller routes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
-// import jobRoute from "./routes/job.route.js";
+import jobRoute from "./routes/job.route.js";
 // import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 3000;
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
-// app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/job", jobRoute);
 // app.use("/api/v1/application", applicationRoute);
 
 
