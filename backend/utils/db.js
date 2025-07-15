ec4eac6d9478a3d26084554581e45002a33f310c
Sainathreddy <sai65265@gmail.com>
2025-07-15 06:09:38 +0530

    backend added with complete

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connected to MongoDB");
+        console.log('mongodb connected successfully');
     } catch (error) {
         console.log(error);
     }
-};
+}
 export default connectDB;
