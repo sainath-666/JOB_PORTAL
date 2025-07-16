5d1344772a2aa5533367135d7d77f84b3edd1136
Sainathreddy <sai65265@gmail.com>
2025-07-17 03:14:14 +0530

    preparing home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,17 @@ import Navbar from './components/shared/Navbar'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import Home from './components/Home'
-import Jobs from './components/Jobs'
-import Browse from './components/Browse'
-import Profile from './components/Profile'
-import JobDescription from './components/JobDescription'
-import Companies from './components/admin/Companies'
-import CompanyCreate from './components/admin/CompanyCreate'
-import CompanySetup from './components/admin/CompanySetup'
-import AdminJobs from "./components/admin/AdminJobs";
-import PostJob from './components/admin/PostJob'
-import Applicants from './components/admin/Applicants'
-import ProtectedRoute from './components/admin/ProtectedRoute'
+// import Jobs from './components/Jobs'
+// import Browse from './components/Browse'
+// import Profile from './components/Profile'
+// import JobDescription from './components/JobDescription'
+// import Companies from './components/admin/Companies'
+// import CompanyCreate from './components/admin/CompanyCreate'
+// import CompanySetup from './components/admin/CompanySetup'
+// import AdminJobs from "./components/admin/AdminJobs";
+// import PostJob from './components/admin/PostJob'
+// import Applicants from './components/admin/Applicants'
+// import ProtectedRoute from './components/admin/ProtectedRoute'
 
 
 const appRouter = createBrowserRouter([
