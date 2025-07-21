13f2288742ac229683bca114971e02168c2ac333
Sainathreddy <sai65265@gmail.com>
2025-07-22 03:07:18 +0530

    profile is done

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react'
 import Navbar from './shared/Navbar'
 import Job from './Job';
 import { useDispatch, useSelector } from 'react-redux';
-import { setSearchedQuery } from '@/redux/jobSlice';
-import useGetAllJobs from '@/hooks/useGetAllJobs';
+// import { setSearchedQuery } from '@/redux/jobSlice';
+// import useGetAllJobs from '@/hooks/useGetAllJobs';
 
 // const randomJobs = [1, 2,3,4,5];
 
