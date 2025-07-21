765d0c62fcf93299b9839e150542fede007ecccf
Sainathreddy <sai65265@gmail.com>
2025-07-22 02:16:50 +0530

    added CategoryCarousel

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
-// import CategoryCarousel from './CategoryCarousel'
+import CategoryCarousel from './CategoryCarousel'
 // import LatestJobs from './LatestJobs'
 // import Footer from './shared/Footer'
 // import useGetAllJobs from '@/hooks/useGetAllJobs'
@@ -21,9 +21,9 @@ const Home = () => {
     <div>
       <Navbar />
       <HeroSection />
-      {/* <CategoryCarousel />
-      <LatestJobs />
-      <Footer /> */}
+      <CategoryCarousel />
+      {/* <LatestJobs /> */}
+      {/* <Footer /> */}
     </div>
   )
 }
