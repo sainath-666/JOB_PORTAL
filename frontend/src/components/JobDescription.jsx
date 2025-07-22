3e917e1d982dd5a9db6daa9f59eda8ca50035756
Sainathreddy <sai65265@gmail.com>
2025-07-23 03:43:05 +0530

    created a hook

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -4,7 +4,7 @@ import { Button } from './ui/button'
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { APPLICATION_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant';
-// import { setSingleJob } from '@/redux/jobSlice';
+import { setSingleJob } from '@/redux/jobSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'sonner';
 
