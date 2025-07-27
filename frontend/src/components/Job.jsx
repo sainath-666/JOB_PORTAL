45c861890fca6f0b46a689a07266cbbe68c6f462
Sainathreddy <sai65265@gmail.com>
2025-07-28 00:48:35 +0530

    added files

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Badge } from './ui/badge'
 import { Button } from './ui/button'
 import { Bookmark } from 'lucide-react'
-import { Avatar, AvatarImage } from './ui/avatar'
-import { Badge } from './ui/badge'
 import { useNavigate } from 'react-router-dom'
+import { ApplyJobDialog } from './ApplyJobDialog'
+import { Avatar, AvatarImage } from './ui/avatar'
 
-const Job = ({job}) => {
-    const navigate = useNavigate();
-    // const jobId = "lsekdhjgdsnfvsdkjf";
+const Job = ({ job }) => {
+    const [open, setOpen] = useState(false);
+    const navigate = useNavigate(); 
 
     const daysAgoFunction = (mongodbTime) => {
         const createdAt = new Date(mongodbTime);
-        const currentTime = new Date();
-        const timeDifference = currentTime - createdAt;
-        return Math.floor(timeDifference/(1000*24*60*60));
+        const currentDate = new Date();
+        const timeDifference = currentDate - createdAt;
+        return Math.floor(timeDifference / (1000 * 24 * 3600));
     }
-    
+ 
     return (
         <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100'>
             <div className='flex items-center justify-between'>
-                <p className='text-sm text-gray-500'>{daysAgoFunction(job?.createdAt) === 0 ? "Today" : `${daysAgoFunction(job?.createdAt)} days ago`}</p>
-                <Button variant="outline" className="rounded-full" size="icon"><Bookmark /></Button>
+                <p className='text-sm text-gray-500'>{daysAgoFunction(job?.createdAt) === 0 ? 'Today' : `${daysAgoFunction(job?.createdAt)} days ago`}</p>
+                <Button size="icon" className="rounded-full" variant="secondary"><Bookmark /></Button>
             </div>
-
             <div className='flex items-center gap-2 my-2'>
-                <Button className="p-6" variant="outline" size="icon">
+                <Button size='icon' variant="outline" className="p-6">
                     <Avatar>
                         <AvatarImage src={job?.company?.logo} />
                     </Avatar>
@@ -34,19 +34,21 @@ const Job = ({job}) => {
                     <p className='text-sm text-gray-500'>India</p>
                 </div>
             </div>
-
             <div>
                 <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
                 <p className='text-sm text-gray-600'>{job?.description}</p>
             </div>
             <div className='flex items-center gap-2 mt-4'>
-                <Badge className={'text-blue-700 font-bold'} variant="ghost">{job?.position} Positions</Badge>
-                <Badge className={'text-[#F83002] font-bold'} variant="ghost">{job?.jobType}</Badge>
-                <Badge className={'text-[#7209b7] font-bold'} variant="ghost">{job?.salary}LPA</Badge>
+                <Badge className={'text-blue-700 font-bold'} variant={'ghost'}>{job?.position} positons</Badge>
+                <Badge className={'text-[#F83002] font-bold'} variant={'ghost'}>{job?.jobType}</Badge>
+                <Badge className={'text-[#7209b7] font-bold'} variant={'ghost'}>{job?.salary}LPA</Badge>
             </div>
             <div className='flex items-center gap-4 mt-4'>
-                <Button onClick={()=> navigate(`/description/${job?._id}`)} variant="outline">Details</Button>
-                <Button className="bg-[#7209b7]">Save For Later</Button>
+                <Button onClick={() => navigate(`/description/${job?._id}`)} variant="outline" className="rounded-lg">Details</Button>
+                <Button className="bg-[#7209b7] rounded-lg">Save For Later</Button>
+            </div>
+            <div>
+                <ApplyJobDialog open={open} setOpen={setOpen} />
             </div>
         </div>
     )
