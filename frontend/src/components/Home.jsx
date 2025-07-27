ed4c52c798c1a469cd649dd258e50b734eeec8d6
Sainathreddy <sai65265@gmail.com>
2025-07-28 00:46:11 +0530

    added files

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,31 +1,32 @@
 import React, { useEffect } from 'react'
-import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
-import CategoryCarousel from './CategoryCarousel'
+import { CategoryCarousel } from './CategoryCarousel'
 import LatestJobs from './LatestJobs'
 import Footer from './shared/Footer'
-import useGetAllJobs from '@/hooks/useGetAllJobs'
+import Navbar from './shared/Navbar'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import useGetAllJobs from '@/hooks/useGetAllJobs'
+
 
 const Home = () => {
-  useGetAllJobs();
-  const { user } = useSelector(store => store.auth);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (user?.role === 'recruiter') {
-      navigate("/admin/companies");
-    }
-  }, []);
-  return (
-    <div>
-      <Navbar />
-      <HeroSection />
-      <CategoryCarousel />
-      <LatestJobs />
-      <Footer />
-    </div>
-  )
+    useGetAllJobs();
+    const { authUser } = useSelector(store => store.auth);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (authUser?.role === 'recruiter') {
+            navigate("/admin/companies");
+        }
+    },[])
+    return (
+        <div>
+            <Navbar />
+            <HeroSection />
+            <CategoryCarousel />
+            <LatestJobs />
+            <Footer />
+        </div>
+    )
 }
 
 export default Home
