9edc8606fb2506858e3c3d9f58bccafa9c80c75e
Sainathreddy <sai65265@gmail.com>
2025-07-28 00:45:34 +0530

    added files

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,59 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import { RadioGroup, RadioGroupItem } from './ui/radio-group'
-import { Label } from './ui/label'
-import { useDispatch } from 'react-redux'
-// import { setSearchedQuery } from '@/redux/jobSlice'
+import React, { useEffect, useState } from 'react';
+import { Label } from './ui/label';
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { useDispatch } from 'react-redux';
+import { setSearchText } from '@/redux/jobSlice';
 
-const fitlerData = [
+const filterData = [
     {
-        fitlerType: "Location",
-        array: ["Delhi NCR", "Bangalore", "Hyderabad", "Pune", "Mumbai"]
+        filterType: "Location",
+        array: ["Delhi NCR", "Bangalore", "Hyderabad", "Pune", "Chennai", "Mumbai"]
     },
     {
-        fitlerType: "Industry",
-        array: ["Frontend Developer", "Backend Developer", "FullStack Developer"]
+        filterType: "Industry",
+        array: ["Frontend Developer", "Backend Developer", "Data Science", "FullStack Developer", "Nextjs Developer"]
     },
     {
-        fitlerType: "Salary",
-        array: ["0-40k", "42-1lakh", "1lakh to 5lakh"]
+        filterType: "Salary",
+        array: ["0 - 40k", "42k to 1lakh", "1lakh to 5lakh"]
     },
-]
+];
 
 const FilterCard = () => {
     const [selectedValue, setSelectedValue] = useState('');
     const dispatch = useDispatch();
-    const changeHandler = (value) => {
+    const handleChange = (value) => {
         setSelectedValue(value);
-    }
-    useEffect(()=>{
-        dispatch(setSearchedQuery(selectedValue));
-    },[selectedValue]);
+    };
+    useEffect(() => {
+        dispatch(setSearchText(selectedValue));
+    }, [selectedValue])
+
     return (
         <div className='w-full bg-white p-3 rounded-md'>
-            <h1 className='font-bold text-lg'>Filter Jobs</h1>
+            <div className='flex items-center justify-between'>
+                <h1 className='font-bold text-lg'>Filter Jobs</h1>
+            </div>
             <hr className='mt-3' />
-            <RadioGroup value={selectedValue} onValueChange={changeHandler}>
-                {
-                    fitlerData.map((data, index) => (
-                        <div>
-                            <h1 className='font-bold text-lg'>{data.fitlerType}</h1>
-                            {
-                                data.array.map((item, idx) => {
-                                    const itemId = `id${index}-${idx}`
-                                    return (
-                                        <div className='flex items-center space-x-2 my-2'>
-                                            <RadioGroupItem value={item} id={itemId} />
-                                            <Label htmlFor={itemId}>{item}</Label>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                    ))
-                }
+            <RadioGroup value={selectedValue} onValueChange={handleChange}>
+                {filterData.map((data, index) => (
+                    <div key={index}>
+                        <h1 className='font-medium text-lg'>{data.filterType}</h1>
+                        {data.array.map((item, idx) => {
+                            const itemId = `r${index}-${idx}`; // Ensure unique id for each radio button
+                            return (
+                                <div key={idx} className="flex items-center space-x-2 my-2">
+                                    <RadioGroupItem value={item} id={itemId} />
+                                    <Label htmlFor={itemId}>{item}</Label>
+                                </div>
+                            );
+                        })}
+                    </div>
+                ))}
             </RadioGroup>
         </div>
-    )
-}
+    );
+};
 
-export default FilterCard
+export default FilterCard;
