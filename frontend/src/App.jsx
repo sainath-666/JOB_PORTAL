f29216f558a82a1848413b5ba69b4a2ebc3e3ff1
Sainathreddy <sai65265@gmail.com>
2025-07-28 01:28:26 +0530

    modifed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,24 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Navbar from './components/shared/Navbar'
-import Login from './components/auth/Login'
-import Signup from './components/auth/Signup'
-import Home from './components/Home'
+import './App.css'
 import Jobs from './components/Jobs'
-import Browse from './components/Browse'
-import Profile from './components/Profile'
+import Home from './components/Home'
 import JobDescription from './components/JobDescription'
-// import Companies from './components/admin/Companies'
-// import CompanyCreate from './components/admin/CompanyCreate'
-// import CompanySetup from './components/admin/CompanySetup'
-// import AdminJobs from "./components/admin/AdminJobs";
-// import PostJob from './components/admin/PostJob'
-// import Applicants from './components/admin/Applicants'
-// import ProtectedRoute from './components/admin/ProtectedRoute'
-
+import Login from './components/auth/Login'
+import Singup from './components/auth/Singup'
+import Profile from './components/Profile'
+import PostJob from './components/PostedJobs'
+import Companies from './components/Companies'
+import CompanySetup from './components/CompanySetup'
+import CompanyCreate from './components/CompanyCreate'
+import Browse from './components/Browse'
+import CreateJobs from './components/admin/CreateJobs'
+import ProtectedRoute from './components/admin/ProtectedRoute'
+import Applicants from './components/admin/Applicants'
 
 const appRouter = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home />
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/signup',
-    element: <Signup />
   },
   {
     path: "/jobs",
@@ -42,38 +33,45 @@ const appRouter = createBrowserRouter([
     element: <Browse />
   },
   {
-    path: "/profile",
-    element: <Profile />
+    path: "/login",
+    element: <Login />
   },
-  // admin ke liye yha se start hoga
-  // {
-  //   path:"/admin/companies",
-  //   element: <ProtectedRoute><Companies/></ProtectedRoute>
-  // },
-  // {
-  //   path:"/admin/companies/create",
-  //   element: <ProtectedRoute><CompanyCreate/></ProtectedRoute> 
-  // },
-  // {
-  //   path:"/admin/companies/:id",
-  //   element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
-  // },
-  // {
-  //   path:"/admin/jobs",
-  //   element:<ProtectedRoute><AdminJobs/></ProtectedRoute> 
-  // },
-  // {
-  //   path:"/admin/jobs/create",
-  //   element:<ProtectedRoute><PostJob/></ProtectedRoute> 
-  // },
-  // {
-  //   path:"/admin/jobs/:id/applicants",
-  //   element:<ProtectedRoute><Applicants/></ProtectedRoute> 
-  // },
-
+  {
+    path: "/signup",
+    element: <Singup />
+  },
+  {
+    path:"/profile",
+    element: <Profile/>
+  },
+  // Admin Dashboard Route Started
+  {
+    path:"/admin/jobs",
+    element:<ProtectedRoute><PostJob/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/jobs/create",
+    element:<ProtectedRoute><CreateJobs/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/jobs/:id/applicants",
+    element:<ProtectedRoute><Applicants/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/companies",
+    element:<ProtectedRoute><Companies/></ProtectedRoute>
+  },
+  {
+    path:"/admin/companies/create",
+    element:<ProtectedRoute><CompanyCreate/></ProtectedRoute> 
+  },
+  {
+    path:"/admin/companies/:id",
+    element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
+  },
 ])
-function App() {
 
+function App() {
   return (
     <div>
       <RouterProvider router={appRouter} />
